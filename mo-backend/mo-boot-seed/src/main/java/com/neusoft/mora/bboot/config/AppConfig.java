05dde5b748fe4b6ec4ef56d7e00d1ad38aa3dbd2
puras <puras.he@gmail.com>
2017-10-12 10:26:20 +0800

    清理无用代码；

--- mo-backend/mo-boot-seed/src/main/java/com/neusoft/mora/bboot/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.mora.bboot.config;
-
-/**
- * @author <a href="mailto:hegq@neusoft.com">puras</a>
- * @version $Revision 1.0 $
- * Created On 2017-08-02 10:31
- */
-public class AppConfig {
-}
--- mo-backend/mo-boot-seed/src/main/java/com/neusoft/mora/bboot/service/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author <a href="mailto:hegq@neusoft.com">puras</a>
- * @version $Revision 1.0 $
- * Created On 2017-08-01 15:06
- */
-package com.neusoft.mora.bboot.service;
